33fe19f37fd6441547b0ace141010de13eb5b186
Kevin-Rey Laguerre <primero@kevin-reys-air.myfiosgateway.com>
2023-07-15 16:32:38 -0400

    Finished login method for accessing instagram account

--- InstagramFollower/main.py
+++ InstagramFollower/main.py
@@ -1,12 +1,35 @@
-from selenium import webdriver 
+from selenium import webdriver  
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+import time
+ 
+SIMILAR_ACCOUNT="codingdojodotco" 
+USERNAME="onparadiscoding"
+PASSWORD="***********"
+
 
 class InstaFollower():  
-    def __init__(self, path) -> None:
-         self.driver = webdriver.Chrome(executable_path=path) 
+    def __init__(self) -> None:
+         self.driver = webdriver.Chrome() 
     
-    def login(): 
-        pass 
-    
+    def login(self):  
+        self.driver.get("https://www.instagram.com/accounts/login/") 
+        self.driver.maximize_window() # For maximizing window
+        self.driver.implicitly_wait(20) # gives an implicit wait for 20 seconds to prevent attempting to find next element before page load
+        username=self.driver.find_element(By.NAME, "username")
+        password=self.driver.find_element(By.NAME, "password") 
+
+
+        username.send_keys(USERNAME) 
+        password.send_keys(PASSWORD) 
+
+        time.sleep(5)
+        password.send_keys(Keys.ENTER)
+        save_info = self.driver.find_element(By.XPATH, "//*[@id='react-root']/section/main/div/div/div/div/button")
+        save_info.click() #Clicks the login button
+
     def find_followers(): 
         pass 
 
@@ -17,5 +40,3 @@ findFollowers = InstaFollower()
 
 #Call three class methods to login and add new followers. 
 findFollowers.login() 
-findFollowers.find_followers() 
-findFollowers.follow()
